b8f4a2304d09df7cc58da383d6c28ccbf4392cc3
Carlos Padilla <carlospadilla91@users.noreply.github.com>
2023-02-22 15:15:49 -0600

    first commit

---
+++ src/test/java/com/safeStopsProvider/SafeStopsProvider/SafeStopsProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeStopsProvider.SafeStopsProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeStopsProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
